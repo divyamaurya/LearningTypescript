23a8f513288bb645ac118d115ea30c617a6b5d3c
Divya Maurya <divyamaurya025@gmail.com>
2024-10-09 17:25:50 +0530

    private, public variables and methods

--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -11,12 +11,34 @@
 // }
 
 class User {
-  private readonly city: string = "Varanasi";
+  private _courseCount = 1;
+
+  readonly city: string = "Varanasi";
   constructor(
     public email: string,
-    public name: string
-  ) // private userId: number
-  {}
+    public name: string // private userId: number
+  ) {}
+
+  //private method
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+  //   getter
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    // set should return nothing
+    if (courseNum <= 1) {
+      throw new Error("Course count should not be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
 
 //creating obj
@@ -24,3 +46,5 @@ const divya = new User("d@f.com", "divya");
 // divya.city = 2;
 // divya.city = "Varanasi"; //readonly
 // divya.city not accessible as private accessible within class
+
+// divya.deleteToken() Property 'deleteToken' is private and only accessible within class 'User'
